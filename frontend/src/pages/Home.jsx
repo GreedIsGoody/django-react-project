f57dced35e531784890bcfae38ca7ed0d4339f92
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-03-17 14:22:01 +0200

    Fixing route and develop a register and login pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import {useState, useEffect} from  'react'
+import api from '../api'
+
+
 function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState('');
+    const [title,setTitle] = useState('');
+
+    
     return <div>Home</div>
 }
 export default Home
